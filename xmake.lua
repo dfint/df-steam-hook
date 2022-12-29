97098cf0eeb1c8d65bb2eaca931fa602edb3bc55
insolor <insolor@gmail.com>
2022-12-29 21:57:12 +0300

    Comment set_targetdir to make github actions work

--- xmake.lua
+++ xmake.lua
@@ -11,7 +11,7 @@ add_requires("vcpkg::detours")
 target("df-hook")
     set_kind("shared")
     set_basename("df-steam-translate-hook")
-    set_targetdir("E:\\Games\\Dwarf Fortress Steam 50.04\\") -- build to DF dir, handy for testing
+    -- set_targetdir("E:\\Games\\Dwarf Fortress Steam 50.04\\") -- build to DF dir, handy for testing
     set_pcxxheader("src/pch.h")
     add_files("src/*.cpp")
     add_packages("spdlog")
