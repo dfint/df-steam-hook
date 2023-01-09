43063d0679342818c6caa21f844259f2c32f19e8
insolor <insolor@gmail.com>
2023-01-09 18:08:44 +0000

    Fix release script

--- ci/release.ps1
+++ ci/release.ps1
@@ -9,15 +9,13 @@ if ($zip_path) {
   $zip_path = ".\dfint_release\dfint_release.zip"
 }
 
-echo $zip_path
-
 xmake -y --all
 mkdir ".\dfint_release\"
 mkdir ".\dfint_release\dfint_data\"
-mkdir ".\dfint_release\dfint_data\offsets\"
 cpi ".\build\windows\x64\release\dfint_launcher.exe" ".\dfint_release\dfint_launcher.exe"
 cpi ".\build\windows\x64\release\dfint_hook.dll" ".\dfint_release\dfint_data\dfint_hook.dll"
-cpi ".\config\" ".\dfint_release\dfint_data\" -Recurse
+cpi ".\config\dfint_config.toml" ".\dfint_release\dfint_data\"
+cpi ".\config\offsets" ".\dfint_release\dfint_data\" -Recurse
 Compress-Archive -Path ".\dfint_release\" -DestinationPath $zip_path
 
 if ($delete_temp) {
