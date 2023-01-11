7cdaa2af5889a883fa2721cbc2df4f47bc79d5d2
insolor <insolor@gmail.com>
2023-01-12 01:16:17 +0300

    Exclude root dfint_release directory from the release package

--- ci/package.ps1
+++ ci/package.ps1
@@ -16,7 +16,7 @@ cpi ".\build\windows\x64\release\dfint_hook.dll" ".\dfint_release\dfint_data\dfi
 cpi ".\config\dfint_config.toml" ".\dfint_release\dfint_data\"
 cpi ".\config\offsets" ".\dfint_release\dfint_data\" -Recurse
 New-Item ".\dfint_release\dfint_data\dfint_dictionary.csv" -type file
-Compress-Archive -Path ".\dfint_release\" -DestinationPath $zip_path
+Compress-Archive -Path ".\dfint_release\*" -DestinationPath $zip_path
 
 if ($delete_temp) {
   rm ".\dfint_release\" -r -force
