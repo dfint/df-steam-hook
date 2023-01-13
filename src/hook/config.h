cdd71080205a5b1a176c607058dd1df0d5374322
insolor <insolor@gmail.com>
2023-01-13 21:06:02 +0300

    Trying to fix hook_version variable

--- src/hook/config.h
+++ src/hook/config.h
@@ -48,9 +48,9 @@ namespace Config {
     inline auto version = Config::offsets["metadata"]["version"].value_or<std::string>("");
     inline auto checksum = Config::offsets["metadata"]["checksum"].value_or<time_t>(0);
 #ifdef HOOK_VERSION
-    inline constexpr int hook_version = HOOK_VERSION;
+    inline auto hook_version = HOOK_VERSION;
 #else
-    inline constexpr int hook_version = 0;
+    inline auto hook_version = "not-defined";
 #endif
 
   }
