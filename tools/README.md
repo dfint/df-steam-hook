13a6df8bbbb3bbaa7fe895d804191fdee2a06df2
insolor <insolor@gmail.com>
2023-05-06 21:15:11 +0300

    Convert notebook into a cli tool

---
+++ tools/README.md
@@ -0,0 +1,13 @@
+# Search patterns
+
+1. Install python 3.10
+
+2. Install poetry:
+    ```
+    pip install poetry
+    ```
+
+3. Run search:
+    ```
+    poetry run search "C:/path/to/Dwarf Fortress/Dwarf Fortress.exe"
+    ```
---
+++ tools/search_offsets/search.py
@@ -0,0 +1,63 @@
+from collections import defaultdict
+from operator import attrgetter
+from pathlib import Path
+from typing import Iterable, Mapping, Optional
+
+import typer
+
+from search_offsets.patterns import (
+    Pattern,
+    check_pattern,
+    group_patterns,
+    load_patterns,
+)
+
+
+def search(path: str, patterns: list[Pattern]):
+    patterns_dict = group_patterns(patterns)
+
+    found = defaultdict(list)
+
+    with open(path, "rb") as file:
+        data = file.read()
+        for i, c in enumerate(data):
+            possible_patterns = patterns_dict.get(c)
+            if possible_patterns:
+                for pattern in possible_patterns:
+                    if check_pattern(data, i, pattern.pattern):
+                        found[pattern.name].append(i)
+
+    return found
+
+
+def print_found(patternt_names: Iterable[str], found: Mapping[str, int]):
+    diff_to_rva = 0xC00
+
+    for pattern in patternt_names:
+        if not found[pattern]:
+            print(f"{pattern}: NOT FOUND")
+            continue
+
+        for i, offset in enumerate(found[pattern], -1):
+            suffix = "" if i < 0 else f"_{i}"
+            name = pattern + suffix
+            if name == "addchar_0":
+                name = "addchar_top"
+            print(f"{name} = 0x{offset + diff_to_rva:X}")
+
+
+app = typer.Typer()
+
+
+@app.command()
+def main(path: Optional[Path] = typer.Option(None)):
+    if path is None:
+        path = Path("/mnt/second/SteamLibrary/steamapps/common/Dwarf Fortress/Dwarf Fortress.exe")
+
+    patterns = load_patterns()
+    found = search(path, patterns)
+    print_found(map(attrgetter("name"), patterns), found)
+
+
+if __name__ == "__main__":
+    app()
